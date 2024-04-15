58ac2346be1478aa32ba73918bfef9d11af76ddf
Rsyd58 <rasyid2007alkautsar@gmail.com>
2024-04-15 21:48:24 +0000

    fix cherry-pick

--- drivers/devfreq/governor_msm_adreno_tz.c
+++ drivers/devfreq/governor_msm_adreno_tz.c
@@ -64,16 +64,10 @@ static u64 suspend_start;
 static unsigned long acc_total, acc_relative_busy;
 
 
-static struct msm_adreno_extended_profile *partner_gpu_profile;
-static void do_partner_start_event(struct work_struct *work);
-static void do_partner_stop_event(struct work_struct *work);
-static void do_partner_suspend_event(struct work_struct *work);
-static void do_partner_resume_event(struct work_struct *work);
+
 
 /* Suspend state boolean */
 static bool suspended = false;
-
-static struct workqueue_struct *workqueue;
 
 
 /*

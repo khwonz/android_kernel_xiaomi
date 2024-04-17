3457ce55c479bf51dd762146c0a55a084c28125a
Rsyd58 <rasyid2007alkautsar@gmail.com>
2024-04-17 13:21:18 +0000

    Revert "drivers: cpu-boost: Set initial boost to 0ms"
    This reverts commit 056f9c6866883cc5dc682b5eeaa1b8d18b14f65e.

--- drivers/cpufreq/cpu-boost.c
+++ drivers/cpufreq/cpu-boost.c
@@ -44,7 +44,7 @@ static struct work_struct input_boost_work;
 
 static bool input_boost_enabled;
 
-static unsigned int input_boost_ms = 0;
+static unsigned int input_boost_ms = 40;
 module_param(input_boost_ms, uint, 0644);
 
 static unsigned int powerkey_input_boost_ms = 400;

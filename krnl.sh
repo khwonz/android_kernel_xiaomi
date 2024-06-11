7e0564511276bc8af868b3df200eeb7a9f134bdf
Rsyd58 <rasyid2007alkautsar@gmail.com>
2024-06-11 03:24:41 +0000

    Updated

--- krnl.sh
+++ krnl.sh
@@ -21,7 +21,7 @@ done
 DEFCONFIG="vince_defconfig"
 export KBUILD_BUILD_USER=Rsyd58
 export TZ=Asia/Jakarta
-export KBUILD_BUILD_HOST=nobody
+export KBUILD_BUILD_HOST=non-pangu-pod
 #export VER="V4.6.15.0.REGMIXM"
 export KERNELDIR="/workspace/Rsyd58/krnlv"
 export USE_CCACHE=1
@@ -35,7 +35,7 @@ export ZIP_DIR="${KERNELDIR}/files"
 export IMAGE="${OUTDIR}/arch/arm64/boot/Image.gz-dtb"
 export ZIPNAME="${KERNELNAME}-vince-$(date +%m%d-%H%M%S).zip"
 export FINAL_ZIP="${ZIP_DIR}/${ZIPNAME}"
-TC_DIR="/workspace/azure-clang"
+TC_DIR="/workspace/weebx"
 export PATH="$TC_DIR/bin:$PATH"
 
 if [[ $1 = "-r" || $1 = "--regen" ]]; then
@@ -49,8 +49,9 @@ rm -rf out
 fi
 
 mkdir -p out
-make O=out ARCH=arm64 $DEFCONFIG 
-export LOCALVERSION="-フリーナ-V4.6.3.0-LA.UM.10.6.2.c26-01500-89xx.0"
+make O=out ARCH=arm64 $DEFCONFIG
+export LOCALVERSION="-msm8953-arm64-Android-12-LAW.BR.5.1.r1"
+export KSU=y
 make -j$(nproc --all) O=out ARCH=arm64  CC=clang HOSTCC=clang HOSTCXX=clang++ READELF=llvm-readelf HOSTAR=llvm-ar AR=llvm-ar AS=llvm-as NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_ARM32=arm-linux-gnueabi-  2>&1 | tee log.txt
 #make -j$(nproc --all) O=out ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_ARM32=arm-linux-gnueabi- 2>&1 | tee log.txt
     echo -e "==========================="
@@ -65,7 +66,7 @@ cp -v "${IMAGE}" "${ANYKERNEL}/";
 cd -;
 cd ${ANYKERNEL};
 zip -r9 ${FINAL_ZIP} *;
-#gh release upload test ${FINAL_ZIP} -R github.com/Rsyd58/Rsyd58;
+gh release upload test ${FINAL_ZIP} -R github.com/Rsyd58/Rsyd58;
 cd -;
 
 #echo -e "zip boot and dtbo"
